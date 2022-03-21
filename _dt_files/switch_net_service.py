e68ba358d27f73868fd3ea29cdb0920a4a2990d8
kaz <kaz>
2022-03-21 04:42:06 -0400

    service

--- _dt_files/switch_net_service.py
+++ _dt_files/switch_net_service.py
@@ -6,8 +6,8 @@ import sys
 
 
 def _p(s):
-    print(s)
-    # otherwise output not shown in journalctl
+    print('[ NET ] LI: {}'.format(s))
+    # o/wise output not shown in journalctl
     sys.stdout.flush()
 
 
@@ -21,28 +21,32 @@ def main():
     # wi-fi can go internet and we already use it
     wlan_has_via = _sh('timeout 2 ping -c 1 -I wlan0 www.google.com')
     if wlan_has_via and _sh('ip route get 8.8.8.8 | grep wlan0'):
-        _p('w')
+        _p('wi-fi')
+        time.sleep(10)
         return
 
     # wi-fi cannot go internet, are we really using it
     if not _sh('/usr/sbin/ifmetric ppp0 400'):
         _p('ifmetric error ppp0 400')
-        return
+        time.sleep(2)
 
     # wi-fi, try again
     if wlan_has_via and _sh('ip route get 8.8.8.8 | grep wlan0'):
-        _p('w*')
+        _p('* wi-fi *')
+        time.sleep(10)
         return
 
     # wi-fi does NOT work, make sure we try cell
     if not _sh('/usr/sbin/ifmetric ppp0 0'):
         _p('ifmetric error ppp0 0')
-        return
+        time.sleep(2)
 
     # check cell can go to internet
     ppp_has_via = _sh('timeout 2 ping -c 1 -I ppp0 www.google.com')
     if ppp_has_via and _sh('ip route get 8.8.8.8 | grep ppp0'):
-        _p('c')
+        _p('cell')
+        # longer
+        time.sleep(300)
         return
 
     _p('-')
@@ -52,4 +56,3 @@ if __name__ == '__main__':
     # see all services -> systemctl list-units --type=service
     while 1:
         main()
-        time.sleep(2)
