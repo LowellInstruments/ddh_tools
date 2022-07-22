18877364d5dc98f0070df2b5d01872df72dcf043
kaz <kaz>
2022-07-22 10:51:08 -0400

    typso

--- dt_update_python_mat.sh
+++ dt_update_python_mat.sh
@@ -7,7 +7,7 @@ VPIP=$VENV/bin/pip
 
 printf '\n\n\n---- Update MAT ----\n'
 
-printf 'U > MAT library...'
+printf '\nU > MAT library...\n'
 source $VENV/bin/activate
 $VPIP install --no-deps --force-reinstall git+https://github.com/LowellInstruments/lowell-mat.git@v4
 
